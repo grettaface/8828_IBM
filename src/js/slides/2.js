59fffe38c04f211844d5c7da85763bb655ef1ac7
grettaface <bret.kruse@gmail.com>
2019-09-18 22:09:09 -0400

    Fix links on slide 2

--- src/js/slides/2.js
+++ src/js/slides/2.js
@@ -11,9 +11,20 @@ export default class slide2 {
       const ct = $(e.currentTarget);
       columns.removeClass('active');
       ct.addClass('active');
-      dotContentDetail.removeClass('active');
-      $(`.dot-content-detail[data-index=${ct.attr('data-index')}]`).addClass(
-        'active'
+      dotContentDetail.hide();
+      TweenMax.to(dotContentDetail, 0.5, {
+        opacity: 0,
+        transform: 'translateY(30px)'
+      });
+
+      $(`.dot-content-detail[data-index=${ct.attr('data-index')}]`).show();
+      TweenMax.to(
+        $(`.dot-content-detail[data-index=${ct.attr('data-index')}]`),
+        0.5,
+        {
+          opacity: 1,
+          transform: 'translateY(0)'
+        }
       );
     });
 
@@ -58,6 +69,6 @@ export default class slide2 {
     });
 
     columns.removeClass('active');
-    dotContentDetail.removeClass('active');
+    dotContentDetail.hide();
   }
 }
